2938707d5e98f7563b13179ca9cf29a8290af81d
5208980 <z5208980@zmail.unsw.edu.au>
2024-10-11 14:21:49 +1100

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import { Metadata } from "next"
 import { Space_Grotesk } from "next/font/google"
-
+import { GoogleAnalytics } from '@next/third-parties/google'
+import { SwayideProviders } from "@/components/providers"
 import { cn } from "@/lib/utils"
 
 import "./globals.css"
-import { SwayideProviders } from "@/components/providers"
 
 export const metadata: Metadata = {
   title: {
@@ -40,6 +40,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <SwayideProviders>
         {children}
       </SwayideProviders>
-    </body >
+    </body>
+    <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS || ""} />
   </html>
 }
